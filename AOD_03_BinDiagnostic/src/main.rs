f1e35aec649531c55d2fe78d9f95d96f5a533832
Nicholas Pellizer <nicholasp21@gmail.com>
2021-12-20 11:28:01 +0100

    add binary string parsing and complete excercise

--- AOD_03_BinDiagnostic/src/main.rs
+++ AOD_03_BinDiagnostic/src/main.rs
@@ -6,12 +6,15 @@ fn main() {
     let contents = fs::read_to_string("input").expect("Something went wrong reading the file");
     let lines = Vec::from_iter(contents.lines());
     let total = lines.len() as u32;
-    let inputSize = lines[0].chars().count();
+    let input_size = lines[0].chars().count();
 
-    let mut counter: Vec<u32> = Vec::with_capacity(inputSize);
+    let mut gamma_rate = String::with_capacity(input_size);
+    let mut counter: Vec<u32> = Vec::with_capacity(input_size);
+
     for i in 0..counter.capacity() {
         counter.push(0)
     }
+
     println!("{:?}", counter);
 
     for elem in contents.lines() {
@@ -23,7 +26,6 @@ fn main() {
         }
     }
 
-    let mut gamma_rate = String::with_capacity(inputSize);
     for bit in counter {
         gamma_rate.push_str(if bit > (total / 2) { "1" } else { "0" });
     }
@@ -31,7 +33,20 @@ fn main() {
     let epsilon_rate = invert_bit_string(&gamma_rate);
     println!("Gamma Rate: {:?}", epsilon_rate);
 
+    let epsilon = convert_string_binary_to_int(&epsilon_rate);
+    let gamma = convert_string_binary_to_int(&gamma_rate);
+    let result = epsilon * gamma;
+
+    println!("Gamma Rate: {}", gamma);
+    println!("Epsilon Rate: {}", epsilon);
+    println!("Final result: {}", result);
+
     // TODO: convert binary string to real number and multiply those
+}
+
+fn convert_string_binary_to_int(val: &str) -> u32 {
+    let intval = u32::from_str_radix(val, 2).unwrap();
+    return intval;
 }
 
 fn invert_bit_string(array: &str) -> std::string::String {
