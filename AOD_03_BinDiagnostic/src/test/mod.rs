36522f21deea1cbe2b3c0d669acd2cdf7041508a
Nicholas Pellizer <nicholasp21@gmail.com>
2021-12-25 21:50:45 +0100

    first unit test ok

--- AOD_03_BinDiagnostic/src/test/mod.rs
+++ AOD_03_BinDiagnostic/src/test/mod.rs
@@ -3,7 +3,7 @@ use std::fs;
 
 #[test]
 fn file_to_lines() {
-    let contents = fs::read_to_string("test/test").expect("Something went wrong reading the file");
+    let contents = fs::read_to_string("test").expect("Something went wrong reading the file");
     let lines = Vec::from_iter(contents.lines());
     
     assert!(lines.len() != 0);
