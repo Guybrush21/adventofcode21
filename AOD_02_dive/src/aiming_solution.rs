9462091e01b1e2fb521e3dfa5c1130c3a1a0a1c9
= <=>
2021-12-31 13:35:20 +0100

    solution 2 refactor

--- AOD_02_dive/src/aiming_solution.rs
+++ AOD_02_dive/src/aiming_solution.rs
@@ -2,12 +2,7 @@ use std::env;
 use std::fs;
 use std::string::String;
 
-pub fn aiming() {
-    let args: Vec<String> = env::args().collect();
-    let filename = &args[1];
-
-    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
-
+pub fn complex_aiming(contents: String) {
     let lines = contents.lines();
     let mut distance = 0;
     let mut depth = 0;
@@ -33,5 +28,31 @@ pub fn aiming() {
     println!("Total depth = {}", depth);
     println!("Total distance = {}", distance);
     println!("Final Aim = {}", aim);
+    println!("Distance x Solution = {}", distance * depth);
+}
+
+pub fn simple_aiming(contents: String) {
+    let lines = contents.lines();
+    let mut distance = 0;
+    let mut depth = 0;
+
+    for elem in lines {
+        let command: Vec<&str> = elem.split_whitespace().collect();
+        let value = command[1].parse::<u32>().unwrap();
+        let direction = command[0];
+
+        if direction.starts_with('f') {
+            distance += value;
+        }
+        if direction.starts_with('u') {
+            depth -= value;
+        }
+        if direction.starts_with('d') {
+            depth += value;
+        }
+    }
+
+    println!("Total depth = {}", depth);
+    println!("Total distance = {}", distance);
     println!("Distance x Solution = {}", distance * depth);
 }
--- AOD_02_dive/src/main.rs
+++ AOD_02_dive/src/main.rs
@@ -1,44 +1,10 @@
-use std::env;
 use std::fs;
-use std::string::String;
+
 mod aiming_solution;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if &args.capacity() > &2 && &args[2] == "simple" {
-        simple_solution();
-    } else {
-        aiming_solution::aiming();
-    }
-}
+    let contents = fs::read_to_string("input").expect("Something went wrong reading the file");
 
-fn simple_solution() {
-    let args: Vec<String> = env::args().collect();
-    let filename = &args[1];
-
-    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
-
-    let lines = contents.lines();
-    let mut distance = 0;
-    let mut depth = 0;
-
-    for elem in lines {
-        let command: Vec<&str> = elem.split_whitespace().collect();
-        let value = command[1].parse::<u32>().unwrap();
-        let direction = command[0];
-
-        if direction.starts_with('f') {
-            distance += value;
-        }
-        if direction.starts_with('u') {
-            depth -= value;
-        }
-        if direction.starts_with('d') {
-            depth += value;
-        }
-    }
-
-    println!("Total depth = {}", depth);
-    println!("Total distance = {}", distance);
-    println!("Distance x Solution = {}", distance * depth);
+    aiming_solution::simple_aiming(contents.clone());
+    aiming_solution::complex_aiming(contents.clone());
 }
