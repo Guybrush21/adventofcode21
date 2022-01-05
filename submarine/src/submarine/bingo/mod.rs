c775e41eb8c08012c7afa10ec81368c265b99692
Nicholas Pellizer <nicholasp21@gmail.com>
2022-01-05 12:35:12 +0100

    board initialization

--- submarine/src/submarine/bingo/mod.rs
+++ submarine/src/submarine/bingo/mod.rs
@@ -1,21 +1,50 @@
-use std::fs;
+use log::debug;
+
 mod tests;
 
-struct Board {
-    rows: Vec<Vec<u8>>,
-    column: Vec<Vec<u8>>,
+#[derive(Debug)]
+pub struct Board {
+    pub rows: Vec<Vec<u8>>,
+    pub columns: Vec<Vec<u8>>,
+}
+
+impl Board {
+    pub fn new(rows: Vec<Vec<u8>>) -> Board {
+        let mut board = Board {
+            rows: rows,
+            columns: Vec::new(),
+        };
+
+        board.create_column();
+
+        board
+    }
+
+    fn create_column(&mut self) {
+        for j in 0..self.rows.len() {
+            let mut col: Vec<u8> = Vec::with_capacity(self.rows.len());
+            for i in 0..self.rows.len() {
+                col.push(self.rows[i][j]);
+            }
+            self.columns.push(col);
+        }
+    }
 }
 
 pub fn resolve(data: String) {
     // read extractions
-    let numbers = extraction(data.clone());
-    
+    let numbers = extraction(&data);
+    let board = build_boards(&data);
+
+    debug!("{:?}", numbers);
+    debug!("{:?}", board);
+
     // read boards
     // calculate winning board
     // calculate win board result
 }
 
-pub fn extraction(data: String) -> Vec<u8> {
+pub fn extraction(data: &str) -> Vec<u8> {
     let raw = data.lines().nth(0).unwrap();
     let extraction: Vec<u8> = raw
         .split(',')
@@ -26,4 +55,39 @@ pub fn extraction(data: String) -> Vec<u8> {
         .collect();
 
     extraction
+}
+
+pub fn build_boards(data: &str) -> Vec<Board> {
+    let mut raw = data.lines().skip(2);
+
+    let mut boards: Vec<Board> = Vec::new();
+
+    while let Some(x) = raw.next() {
+        if x != "" {
+            let r0 = x;
+            let r1 = raw.next().unwrap();
+            let r2 = raw.next().unwrap();
+            let r3 = raw.next().unwrap();
+            let r4 = raw.next().unwrap();
+
+            let raw_rows = vec![r0, r1, r2, r3, r4];
+
+            let mut rows: Vec<Vec<u8>> = Vec::with_capacity(5);
+
+            for r in raw_rows {
+                let row: Vec<u8> = r
+                    .split(' ')
+                    .filter(|x| !x.is_empty())
+                    .map(|x| x.parse::<u8>().unwrap())
+                    .collect();
+
+                rows.push(row);
+            }
+
+            let b = Board::new(rows);
+            boards.push(b);
+        }
+    }
+
+    boards
 }
--- submarine/src/submarine/bingo/tests.rs
+++ submarine/src/submarine/bingo/tests.rs
@@ -2,7 +2,7 @@
 use std::fs;
 use std::{collections::HashSet, hash::Hash};
 
-use crate::submarine::bingo::extraction;
+use crate::submarine::bingo::{build_boards, extraction, Board};
 
 fn vec_equals<T>(a: &[T], b: &[T]) -> bool
 where
@@ -23,7 +23,35 @@ fn extraction_number() {
         7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13, 6, 15, 25, 12, 22, 18, 20, 8, 19, 3,
         26, 1,
     ];
-    let extraction = extraction(contents);
+    let extraction = extraction(&contents);
 
     assert!(vec_equals(&test_case, &extraction))
+}
+
+#[test]
+fn read_board() {
+    let contents =
+        fs::read_to_string("data/04-test").expect("Something went wrong reading the file");
+
+    let mut columns: Vec<Vec<u8>> = Vec::new();
+    columns.push(vec![22, 8, 21, 6, 1]);
+    columns.push(vec![13, 2, 9, 10, 12]);
+    columns.push(vec![17, 23, 14, 3, 20]);
+    columns.push(vec![11, 4, 16, 18, 15]);
+    columns.push(vec![0, 24, 7, 5, 19]);
+
+    let mut rows: Vec<Vec<u8>> = Vec::new();
+    rows.push(vec![22, 13, 17, 11, 0]);
+    rows.push(vec![8, 2, 23, 4, 24]);
+    rows.push(vec![21, 9, 14, 16, 7]);
+    rows.push(vec![6, 10, 3, 18, 5]);
+    rows.push(vec![1, 12, 20, 15, 19]);
+
+    let boards = build_boards(&contents);
+    for i in &boards {
+        println!("{:?}", i)
+    }
+
+    assert!(vec_equals(&boards[0].rows[0], &rows[0]));
+    assert!(vec_equals(&boards[0].columns[0], &columns[0]));
 }
